46cb0958bd10381a76f833aa82435b8c0241b4be
Otoglovi <darlington.2004@gmail.com>
2017-04-17 20:33:00 +0100

    Homepage

--- home.php
+++ home.php
@@ -8,5 +8,20 @@
 <body>
 <h1>Hello World</h1>
 
+<header>
+    <h1>Welcome to Marvel Movies</h1>
+</header>
+<main>
+    <section>
+        <h2>You are now connected to the database</h2>
+
+        <p><a href="allMovies.php">All Marvel Movies</a></p>
+        <p><a href="moviesAfter2010.php">All Marvel Movies Produced after 2010</a></p>
+        <p><a href="xMenMovies.php">All X-MEN Movies</a></p>
+        <p><a href="myBootstrapTheme.html">Simply Me Theme</a></p>
+        <p><a href="norhtLab.html">The North Lab</a></p>
+
+    </section>
+
 </body>
 </html>
